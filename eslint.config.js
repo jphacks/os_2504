27c5ab98b53e50b189e499c827fff92be8e6efe1
rintarooort <rintarooooort@gmail.com>
2025-10-19 06:30:32 +0900

    エラーハンドリング通過用

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
   {
-    ignores: ['dist/**', 'drizzle/**', '.vercel/**', 'node_modules/**']
+    ignores: ['dist/**', 'drizzle/**', '.vercel/**', 'node_modules/**', 'ui_docs/**']
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
